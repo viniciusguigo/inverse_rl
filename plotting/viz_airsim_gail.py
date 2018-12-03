da5b82ffd657ec7c87ae644b6398b2506010ca91
Vinicius G. Goecks <vinicius.goecks@tamu.edu>
2018-12-03 08:48:35 -0600

    added plots

---
+++ plotting/viz_airsim_gail.py
@@ -0,0 +1,120 @@
+# import
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import seaborn as sns
+sns.set(style='whitegrid', font_scale=1.25)
+
+
+def parse_episode(log_data, obs_log_data, epi_n):
+    """ Return states, actions, rewards from a specific episode.
+
+    Args:
+        epi_n : episode numnber
+
+    Return:
+        states : position, velocity, acceleration  (linear and angular)
+        actions : controls applied
+        rewards : rewards received by the agent
+
+    """
+    # parse specific episode
+    steps = np.where(log_data[:,1] == epi_n)
+    steps = np.squeeze(np.asarray(log_data[steps,0])).astype(int)
+
+    # find initial and final index in 
+
+    # parse states and actions
+    # hardcoded to take a (15,) vector for obs and (2,) for actions
+    states = obs_log_data[steps,1:]
+    actions = log_data[steps,3:5]
+    rewards = log_data[steps,7]
+
+    return states, actions, rewards
+
+def plot_history(fig, ax, avg_return, n_samples, run_id_label, avg_factor):
+    """Plot history results (iterations, mean reward) using Matplotlib.
+    """
+    # average data
+    avg_return = avg_return[0]
+    samples = avg_return.shape[0]
+    avg_samples = int(np.floor(samples/avg_factor))    
+    avg = np.zeros((avg_samples,2))
+
+    for i in range(avg_samples):
+        initial_idx = i*avg_factor
+        avg[i,0] = initial_idx + avg_factor
+
+        # remove nan values and compute average
+        # x = x[~numpy.isnan(x)]
+        x = avg_return[initial_idx:initial_idx + avg_factor]
+        x = x[~np.isnan(x)]
+        avg[i,1] = np.mean(x)
+
+    # plot avg
+    ax.plot(avg[:,0], avg[:,1], label=run_id_label)
+
+
+def process_avg(fig, ax, run_id, run_id_label, avg_factor, n_seeds=1):
+    """Load files, calculate average, std dev, plot, and save figure.
+    """
+    # load first file (seed == 1)
+    data = np.genfromtxt('{}/progress.csv'.format(run_id), delimiter=',', skip_header=True)
+    n_samples = data.shape[0]
+    labels = np.genfromtxt('{}/progress.csv'.format(run_id), dtype=str, delimiter=',', max_rows=1)
+
+    # find returns on progress.csv file
+    avg_return_idx = np.where([label == 'OriginalTaskAverageReturn' for label in labels])[0][0]
+    avg_return = data[:,avg_return_idx].reshape(1,n_samples)
+
+    # plot
+    plot_history(fig, ax, avg_return, n_samples, run_id_label, avg_factor)
+
+    return n_samples
+
+if __name__ == '__main__':
+    # setup figure
+    avg_factor = 10
+    save_pic = True
+    name_pic = './plotting/airsim_gail.png'
+
+    # plot rewards
+    fig, ax = plt.subplots(1)
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Original Task Average Return")
+
+    # plot data
+    ## GAIL (v2)
+    run_id = './data/airsim_gail_v2'
+    run_id_label = 'v2'
+    n_samples = process_avg(fig, ax, run_id, run_id_label, avg_factor)
+
+    ## GAIL (v3)
+    run_id = './data/airsim_gail_v3'
+    run_id_label = 'v3'
+    n_samples = process_avg(fig, ax, run_id, run_id_label, avg_factor)
+
+    ## HUMAN
+    log_data = np.genfromtxt('data/airsim_human_data/log.csv', delimiter=',', skip_header=True)
+    obs_log_data = np.genfromtxt('data/airsim_human_data/low_obs_log.csv', delimiter=',', skip_header=True)
+    n_epis = int(np.max(log_data[:,1]))
+    human_data = np.zeros(n_epis)
+
+    for i in range(n_epis):
+        _, _, returns = parse_episode(log_data, obs_log_data, i)
+        human_data[i] = np.sum(returns)
+    
+    # print human mean reward and stddev bounds
+    human_mean = np.mean(human_data)
+    human_stddev = np.std(human_data)
+    ax.hlines(human_mean, 0, n_samples, label='Human Mean')
+    ax.hlines(human_mean+human_stddev, 0, n_samples, linestyle='dotted', label='Human Stddev')
+    ax.hlines(human_mean-human_stddev, 0, n_samples, linestyle='dotted')
+    
+    # save/show pic
+    plt.legend()
+    plt.tight_layout()
+    if save_pic:
+        plt.savefig(name_pic, dpi=300)    
+    plt.show()
+        
--- plotting/viz_exp.py
+++ plotting/viz_exp.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import argparse
 sns.set(style='whitegrid', font_scale=1.25)
 
-def main(data_addr, stat_label):
+def main(data_addr, data_label, stat_label):
     # load data from progress log
     data = np.genfromtxt('{}/progress.csv'.format(data_addr), delimiter=',', skip_header=True)
     labels = np.genfromtxt('{}/progress.csv'.format(data_addr), dtype=str, delimiter=',', max_rows=1)
@@ -13,21 +13,24 @@ def main(data_addr, stat_label):
     avg_return_idx = np.where([label == stat_label for label in labels])[0][0]
 
     # plot them
-    plt.figure()
     plt.ylabel(labels[avg_return_idx])
     plt.xlabel('Iteration')
-    plt.plot(data[:,avg_return_idx])
+    plt.plot(data[:,avg_return_idx], label=data_label)
     plt.xlim([0,data.shape[0]])
     plt.tight_layout()
     plt.legend()
 
 if __name__ == '__main__':
     # define data location
-    data_addr = './plotting/pendulum_final'
+    data_addrs = ['./data/airsim_gail_v2', './data/airsim_gail_v3']
+    data_label = ['v2', 'v3']
 
     # define statistics to plot
-    main(data_addr, 'AverageReturn')
-    main(data_addr, 'Entropy')
+    label_count = 0
+    plt.figure()
+    for data_addr in data_addrs:
+        main(data_addr, data_label[label_count], 'Entropy')
+        label_count += 1
 
     # display at the end
     plt.show()
